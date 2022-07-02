3f85601be0dc5e8689025652a2f4bebfbe1776fc
Pat Trudel <patrick.trudel@me.com>
2022-07-02 10:48:26 -0400

    Initial Commit

---
+++ TrendTok/TrendTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendTokApp.swift
+//  TrendTok
+//
+//  Created by Pat Trudel on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct TrendTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
